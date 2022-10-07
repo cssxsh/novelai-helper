6d109f724d5613b1290a660045139ddf3b4a3e4c
cssxsh <cssxsh@gmail.com>
2022-10-07 20:26:18 +0800

    build: v1.0.1

--- src/main/kotlin/xyz/cssxsh/mirai/novelai/NovelAiHelper.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/novelai/NovelAiHelper.kt
@@ -11,7 +11,7 @@ public object NovelAiHelper : KotlinPlugin(
     JvmPluginDescription(
         id = "xyz.cssxsh.mirai.plugin.novelai-helper",
         name = "novelai-helper",
-        version = "1.0.0",
+        version = "1.0.1",
     ) {
         author("cssxsh")
     }
